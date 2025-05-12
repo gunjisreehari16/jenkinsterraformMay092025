f6f012bbe905c0b9f1ae0c0bf1b2b8dcbd36eec1
gunjisreehari16 <142888692+gunjisreehari16@users.noreply.github.com>
2025-05-12 10:51:28 +0530

    Create jump_server.tf

---
+++ Everythingworkingmay122025/jump_server.tf
@@ -0,0 +1,28 @@
+# Jump Server Instance (public subnet)
+resource "aws_instance" "jump_server" {
+  ami                         = var.jump_server_ami
+  instance_type               = var.jump_server_instance_type
+  subnet_id                   = aws_subnet.RSP_IOT_public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.jump_sg.id]
+  associate_public_ip_address = true
+  key_name                    = var.key_name
+  user_data = templatefile("scripts/jump_server.sh.tpl", {
+  JENKINS_IP = aws_instance.jenkins_master.private_ip
+  })
+
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "Jump Server"
+  }
+}
+
+# EIP for Jump Server
+resource "aws_eip" "jump_server_eip" {
+  domain     = "vpc"
+  instance   = aws_instance.jump_server.id
+  depends_on = [aws_internet_gateway.RSP_IOT_igw]
+}
