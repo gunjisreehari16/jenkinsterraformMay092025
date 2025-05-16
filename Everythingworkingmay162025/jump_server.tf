22ca9aef6a12b0ed74e03809fd9cdc5bc40e75b5
gunjisreehari16 <142888692+gunjisreehari16@users.noreply.github.com>
2025-05-16 08:17:04 +0530

    Add files via upload

---
+++ Everythingworkingmay162025/jump_server.tf
@@ -0,0 +1,29 @@
+# Jump Server Instance (public subnet)
+resource "aws_instance" "jump_server" {
+  ami                         = var.jump_server_ami
+  instance_type               = var.jump_server_instance_type
+  subnet_id                   = aws_subnet.vzrsp_cicd_public_subnet[0].id
+  vpc_security_group_ids      = [aws_security_group.jump_sg.id]
+  associate_public_ip_address = true
+  key_name                    = var.key_name
+
+  user_data = templatefile("scripts/jump_server.sh.tpl", {
+    JENKINS_IP = aws_instance.jenkins_master.private_ip
+  })
+
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "Jump Server"
+  }
+}
+
+# EIP for Jump Server
+resource "aws_eip" "jump_server_eip" {
+  domain     = "vpc"
+  instance   = aws_instance.jump_server.id
+  depends_on = [aws_internet_gateway.vzrsp_cicd_igw]
+}
---
+++ Everythingworkingmay162025/security_groups.tf
@@ -0,0 +1,80 @@
+# Security Group for Jenkins
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins-sg"
+  vpc_id      = aws_vpc.vzrsp_cicd_vpc.id
+  description = "Allow Jenkins traffic"
+
+  # SSH access from Jump Server only
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.jump_sg.id]
+  }
+
+  # HTTP for Jenkins web UI
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+  # Jenkins slave communication
+  ingress {
+    from_port   = 50000
+    to_port     = 50000
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+  # Internal communication within VPC
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "jenkins-sg" }
+}
+
+# Security Group for Jump Server
+resource "aws_security_group" "jump_sg" {
+  name        = "jump-server-sg"
+  vpc_id      = aws_vpc.vzrsp_cicd_vpc.id
+  description = "Allow SSH from internet and access to private subnet"
+
+  # SSH access from the internet (you can restrict to a specific IP/CIDR)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "jump-server-sg" }
+}
